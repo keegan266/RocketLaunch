9161f0cd235f5cc7fcd086690fc610db5eb1374f
Keegan Hart <keeganahart@gmail.com>
2019-10-21 17:26:16 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('next5').addEventListener('click', function() {
+    var launchAry = [];
+    var xhttp = new XMLHttpRequest();
+    
+    launchAry.push(xhttp.open("GET", "https://launchlibrary.net/1.4/launch?next=5", true))
+    
+});
