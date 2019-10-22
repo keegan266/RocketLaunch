b40ab0eda30bed34e57e922e2689c1cbeaf948ec
Keegan Hart <keeganahart@gmail.com>
2019-10-22 11:02:34 -0500

    some progress

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
+var httpRequest = new XMLHttpRequest();
+var launchAry = [];
 document.getElementById('next5').addEventListener('click', function() {
-    var launchAry = [];
-    var xhttp = new XMLHttpRequest();
-    
-    launchAry.push(xhttp.open("GET", "https://launchlibrary.net/1.4/launch?next=5", true))
+
+    httpRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5");
+    httpRequest.send();
+    httpRequest.onreadystatechange = getLaunchInfo;
+
     
 });
+
+function getLaunchInfo() {
+    var nextLaunch = httpRequest.responseText;
+    var list = "";
+    var jsObj = JSON.parse(nextLaunch);
+    
+    function launch(name, date) {
+        this.launchName = name;
+        this.launchDate = date;
+    }
+
+    var launchName = jsObj.name;
+    var launchDate = jsObj.windowstart;
+    const newLaunch = new launch(launchName, launchDate);
+    launchAry.push(newLaunch);
+    console.log(newLaunch);
+}
